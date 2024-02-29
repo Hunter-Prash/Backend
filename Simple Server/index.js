a923617f2177c87bf0978a39b90d3a5863fec4d1
Prashant Rajaji <103595753+Hunter-Prash@users.noreply.github.com>
2024-02-29 18:31:55 +0530

    Add files via upload
    Creating my first server

---
+++ Simple Server/index.js
@@ -0,0 +1,22 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/twitter',(req,res)=>{
+    res.send('Ghost of tsushima')
+})
+
+app.get('/tvshow',(req,res)=>{
+    res.send('<h2>Game of Thrones</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
